ae98a582de419a8fafe5635498bf19c9787a1e1f
root <root@daw2m08.fjeclot.net>
2018-04-23 19:37:12 +0200

    Commit 3

--- peupagina.php
+++ peupagina.php
@@ -1,4 +1,4 @@
-<?php
+<?php add_action('admin_menu','admin_menu_peu_pagina');
 /*
 Plugin Name: Peu de pàgina per defecte per a noves entrades
 Plugin URI: Escriu l'adreça URL completa del teu plugin dins de Github
@@ -13,11 +13,14 @@ $titolPagina="Peu de pàgina per defecte";
 $titolEntrada="Peu de pàgina per defecte";
 $role="administrator";
 $id="peu_pagina_per_defecte";
-$nom_funcio="nouPeuDef";
-add_menu_page($titolPagina,$titolEntrada,$role,$id,$nom_funcio);;
+$nom_funcio="nouPeuPagDef";
+add_menu_page($titolPagina,$titolEntrada,$role,$id,$nom_funcio);
 }
 function nouPeuPagDef(){
 include_once('formPeuPag.php');
 }
-add_action('admin_menu','admin_menu_peu_pagina');
+add_action('wp_footer','afegPeuPagDef');
+function afegPeuPagDef(){
+echo "<div style='color: red;font-size: 10px;margin: 10px;'>".get_option('footer_text')."</div>";
+}
 ?>
